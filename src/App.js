15d67ac12d92498e4d6c685615b7e4853e12f858
Asep Sanusi <asepsanusisan@gmail.com>
2023-11-20 16:44:48 +0700

    - slicing section core values

--- src/App.js
+++ src/App.js
@@ -26,6 +26,20 @@ function App() {
         "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse sequam nihil molestiae consequatur.",
     },
   ];
+  const dataCoreValues = [
+    {
+      title: "Dedication",
+      desc: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat.",
+    },
+    {
+      title: "Intellectual Honesty",
+      desc: "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias conse.",
+    },
+    {
+      title: "Curiosity",
+      desc: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+    },
+  ];
 
   const handleClickLeft = () => {
     if (questionActive !== 1) {
@@ -65,8 +79,10 @@ function App() {
         <div className="clip-outside" />
       </section>
 
-      <section className="question">
-        <img src="/scroll-down.svg" className="scroll-down" alt="icon" />
+      <section className="question" id="__question">
+        <a href="#__question">
+          <img src="/scroll-down.svg" className="scroll-down" alt="icon" />
+        </a>
         <div className="slide">
           {dataQuestion.map((q) => (
             <div className="wrap" key={q.id} id={`question_${q.id}`}>
@@ -90,6 +106,32 @@ function App() {
             </div>
           </div>
         </div>
+      </section>
+
+      <section className="core-values">
+        <h1>Our Core Values</h1>
+        <p className="desc">
+          Ridiculus laoreet libero pretium et, sit vel elementum convallis
+          fames. Sit suspendisse etiam eget egestas. Aliquet odio et lectus
+          etiam sit.
+          <br />
+          <br />
+          In mauris rutrum ac ut volutpat, ornare nibh diam. Montes, vitae, nec
+          amet enim.
+        </p>
+
+        <div className="list">
+          {dataCoreValues.map((cv) => (
+            <div className="content">
+              <div className="line" />
+              <div>
+                <h2>{cv.title}</h2>
+                <p>{cv.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <img src="/illustrator.png" alt="img" />
       </section>
     </div>
   );
